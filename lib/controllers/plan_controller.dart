8c49f5ce86c245b0c940c53cf4a011a5a82b108e
unknown <yonattanz11@gmail.com>
2025-03-30 10:57:39 -0400

    appBar agregado

---
+++ lib/controllers/plan_controller.dart
@@ -0,0 +1,33 @@
+import 'package:get/get.dart';
+
+class PlanController extends GetxController {
+  // Lista de planes con sus características
+
+  // final plans =
+  //     [
+  //       {
+  //         "name": "Basic",
+  //         "features": [
+  //           "Acceso a contenido limitado",
+  //           "Soporte estándar",
+  //           "1 dispositivo",
+  //         ],
+  //       },
+  //       {
+  //         "name": "Plus",
+  //         "features": [
+  //           "Acceso a todo el contenido",
+  //           "Soporte prioritario",
+  //           "Hasta 3 dispositivos",
+  //         ],
+  //       },
+  //       {
+  //         "name": "Business",
+  //         "features": [
+  //           "Funciones avanzadas",
+  //           "Soporte 24/7",
+  //           "Usuarios ilimitados",
+  //         ],
+  //       },
+  //     ].obs;
+}
--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -1,11 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:prueba_uopp/config/config.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home Page')),
+      appBar: AppBar(
+        title: Row(
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.arrow_back_ios, color: AppConfig.bgTextColor),
+            ),
+            Text(
+              AppConfig.selectPlan,
+              textAlign: TextAlign.left,
+
+              style: TextStyle(
+                fontFamily: 'CircularStd',
+                color: AppConfig.bgTextColor,
+                fontSize: 24,
+                // fontWeight: FontWeight.w700,
+              ),
+            ),
+          ],
+        ),
+        flexibleSpace: Container(
+          height: 95,
+          padding: EdgeInsets.fromLTRB(16, 55, 16, 10),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: <Color>[Color(0xffB5AEFF), Color(0xff9688FF)],
+            ),
+          ),
+        ),
+      ),
       body: Center(
         child: ElevatedButton(
           onPressed:
